18ea6c56593b1341a9f99426a48b7edda5bf012d
Tomas Cardona <tomascardona787@gmail.com>
2026-08-16 22:12:59 -0500

    Add comment for PostgreSQL service integration
    Added a comment to indicate pending work for PostgreSQL integration.

--- videojuegos/src/main/java/services/VideojuegoServices.java
+++ videojuegos/src/main/java/services/VideojuegoServices.java
@@ -3,6 +3,6 @@ package services;
 public class VideojuegoServices {
 
     public void registrarProducto(){
-        
+        //pendiente: crear services y terminar de conectarlo con PostgreSQL
     }
 }
